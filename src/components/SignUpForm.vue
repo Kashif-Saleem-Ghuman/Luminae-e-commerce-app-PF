d08c7c2528b82a30aab531ea1a9aca21be55f192
kashif <kashif_saleem_23@hotmail.com>
2023-09-17 14:13:37 +0500

    Refac: Renamed component to sign up form

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,125 @@
+<template>
+  <section class="container forms">
+    <div class="form login">
+      <h1>{{ props.title || props.defaultText }}</h1>
+      <form id="LoginForm" action="#">
+        <div class="field input-field">
+          <input
+            type="text"
+            placeholder="username"
+            id="email"
+            class="input"
+            v-model="username"
+          />
+        </div>
+
+        <div class="field input-field">
+          <input
+            type="password"
+            placeholder="Password"
+            id="passwordLogin"
+            class="password"
+            v-model="password"
+          />
+          <i class="bx bxs-hide eye-icon"></i>
+        </div>
+        <div class="btn-wrapper">
+          <div class="field">
+            <button @click="submitForm" class="button-login">Login</button>
+          </div>
+
+          <div class="button-field-sign-up">
+            <span style="color: red">New User?</span>
+            <router-link to="/">Sign Up</router-link>
+          </div>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { defineProps } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+
+// refs
+const user = ref(null);
+const router = useRouter();
+
+// state
+const username = ref("");
+const password = ref("0lelplR");
+
+// create an async function to fetch the api usig axios
+
+const getUserData = async () => {
+  try {
+    const response = await axios.post("https://dummyjson.com/auth/login", {
+      username: username.value,
+      password: password.value,
+    });
+    const data = await response.data;
+    //store data in user ref
+    user.value = data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Toast notifications
+const toastNotificationsError = (message) => {
+  toast.error(message, {
+    position: "top-center",
+    duration: 3000,
+  });
+};
+//Function to validate form fields
+const valditeFormInputs = async () => {
+  await getUserData();
+  if (username.value === "" || password.value === "") {
+    // alert("please fill out all the fields");
+    //send a toast error message
+    toastNotificationsError("please fill out all the fields");
+    return false;
+  } else {
+    return true;
+  }
+};
+// Function to set the user data to local storage
+const storeDataToLocalStorage = (name, data) => {
+  localStorage.setItem(name, JSON.stringify(data));
+};
+
+//Function to redirect to page
+const redirectToPage = (page) => {
+  router.push(page);
+};
+
+//Function to submit the form
+const submitForm = async (e) => {
+  e.preventDefault();
+  // if validation is successful then store the data to local storage
+  const isValid = await valditeFormInputs();
+  console.log("isValid", isValid);
+  // check if validateFormInputs is true then store the data to local storage and redirect to page
+  //please not since the the username and password are harcoded in the refs so isValid will always be true
+  if (isValid) {
+    console.log("I am working");
+    storeDataToLocalStorage("UserData", user.value);
+    redirectToPage("/product-categories");
+  }
+};
+
+// Props
+const props = defineProps({
+  title: String,
+  defaultText: {
+    type: String,
+    default: "Login asdasd",
+  },
+});
+</script>
