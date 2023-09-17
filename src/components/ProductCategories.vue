8749975ccf5c1c679939e56bbc3455fc38fcada5
kashif <kashif_saleem_23@hotmail.com>
2023-09-17 14:13:12 +0500

    Feat: Added Product caegories component

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -1,3 +1,34 @@
 <template>
-  <div>Product Categories</div>
+  <h1>Name is {{ productCategories.name }}</h1>
+  <input
+    type="text"
+    placeholder="name of product"
+    v-model="productCategoryName"
+  />
+  <button @click="handleSubmit">Submit</button>
 </template>
+
+<script setup>
+import { useProductCategoriesStore } from "@/stores/ProductsCategoriesStore";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+
+const productCategoriesStore = useProductCategoriesStore();
+
+const { productCategories } = storeToRefs(productCategoriesStore);
+console.log("State: ", productCategories);
+// ref for the form
+const productCategoryName = ref("");
+
+//destructuring the store
+productCategoriesStore;
+
+// destructuring the store action
+const { addProductCategory } = productCategoriesStore;
+
+// function to handle the submit
+const handleSubmit = () => {
+  addProductCategory(productCategoryName.value);
+  productCategoryName.value = "";
+};
+</script>
