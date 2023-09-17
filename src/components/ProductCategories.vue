ce673820d1380fd95c3aeec849f9840a42439019
kashif <kashif_saleem_23@hotmail.com>
2023-09-17 18:00:16 +0500

    Refac: Refactored to consume the user state

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -9,9 +9,15 @@
 </template>
 
 <script setup>
+import { useUserlogin } from "@/stores/UserLogin";
+
 import { useProductCategoriesStore } from "@/stores/ProductsCategoriesStore";
 import { storeToRefs } from "pinia";
 import { ref } from "vue";
+
+const userLoginData = useUserlogin();
+const { user } = storeToRefs(userLoginData);
+console.log("User: ", user);
 
 const productCategoriesStore = useProductCategoriesStore();
 
@@ -20,8 +26,7 @@ console.log("State: ", productCategories);
 // ref for the form
 const productCategoryName = ref("");
 
-//destructuring the store
-productCategoriesStore;
+//destructuring the store productCategoriesStore;
 
 // destructuring the store action
 const { addProductCategory } = productCategoriesStore;
@@ -31,4 +36,6 @@ const handleSubmit = () => {
   addProductCategory(productCategoryName.value);
   productCategoryName.value = "";
 };
+
+console.log()
 </script>
