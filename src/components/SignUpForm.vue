27a9304bdf236f9c8dc439fe45d2b46c09cf2bb5
kashif <kashif_saleem_23@hotmail.com>
2023-09-17 17:59:53 +0500

    Refac: Refactored to use the state from user store

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -25,7 +25,9 @@
         </div>
         <div class="btn-wrapper">
           <div class="field">
-            <button @click="submitForm" class="button-login">Login</button>
+            <button @click="submitForm" class="button-login">
+              {{ user.isLoading ? "Loading..." : "Login" }}
+            </button>
           </div>
 
           <div class="button-field-sign-up">
@@ -41,46 +43,47 @@
 <script setup>
 import { ref } from "vue";
 import { defineProps } from "vue";
-import axios from "axios";
 import { useRouter } from "vue-router";
 import { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
+import { useUserlogin } from "@/stores/UserLogin";
+import { storeToRefs } from "pinia";
 
-// refs
-const user = ref(null);
+const userLoginData = useUserlogin();
 const router = useRouter();
+
+//destructuring the store state
+const { user } = storeToRefs(userLoginData);
+
+// destructuring the store actions
+const { fetchUserData } = userLoginData;
+// refs
 
 // state
 const username = ref("kminchelle");
 const password = ref("0lelplR");
 
-// create an async function to fetch the api usig axios
-
-const getUserData = async () => {
-  try {
-    const response = await axios.post("https://dummyjson.com/auth/login", {
-      username: username.value,
-      password: password.value,
-    });
-    const data = await response.data;
-    //store data in user ref
-    user.value = data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-// Toast notifications
+// Toast notifications for error
 const toastNotificationsError = (message) => {
   toast.error(message, {
     position: "top-center",
     duration: 3000,
+    closeButton: true,
+  });
+};
+
+// Toast notifications for success
+const toastNotificationsSuccess = (message) => {
+  toast.success(message, {
+    position: "top-center",
+    duration: 1000,
+    closeButton: true,
   });
 };
 
 //Function to validate form fields
 const valditeFormInputs = async () => {
-  await getUserData();
+  await fetchUserData(username.value, password.value);
   if (username.value === "" || password.value === "") {
     // alert("please fill out all the fields");
     //send a toast error message
@@ -90,14 +93,17 @@ const valditeFormInputs = async () => {
     return true;
   }
 };
+
 // Function to set the user data to local storage
 const storeDataToLocalStorage = (name, data) => {
   localStorage.setItem(name, JSON.stringify(data));
 };
 
-//Function to redirect to page
+// Function to redirect to page
 const redirectToPage = (page) => {
-  router.push(page);
+  setTimeout(() => {
+    router.push(page);
+  }, 5000);
 };
 
 //Function to submit the form
@@ -105,11 +111,11 @@ const submitForm = async (e) => {
   e.preventDefault();
   // if validation is successful then store the data to local storage
   const isValid = await valditeFormInputs();
-  console.log("isValid", isValid);
   // check if validateFormInputs is true then store the data to local storage and redirect to page
   //please not since the the username and password are harcoded in the refs so isValid will always be true
   if (isValid) {
     storeDataToLocalStorage("UserData", user.value);
+    toastNotificationsSuccess("Sign Up Successful");
     redirectToPage("/product-categories");
   }
 };
